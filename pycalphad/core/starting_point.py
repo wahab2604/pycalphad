9190061150cc81811958a472ffd2c69aee387b42
Richard Otis <richard.otis@jpl.nasa.gov>
2019-04-10 10:49:38 -0700

    WIP: starting_point: Increase max number of phases allowed on grid when state variables are free. This will allow non-global-min-based starting points.

--- pycalphad/core/starting_point.py
+++ pycalphad/core/starting_point.py
@@ -89,22 +89,18 @@ def starting_point(conditions, state_variables, phase_records, grid):
     if len(dependent_comp) != 1:
         raise ValueError('Number of dependent components different from one')
 
-    arrays = {'NP':     (conds_as_strings + ['vertex'], np.empty(grid_shape + (len(nonvacant_elements)+1,))),
+    arrays = {'NP':     (conds_as_strings + ['vertex'], np.empty(grid_shape + (max_phases,))),
               'GM':     (conds_as_strings, np.empty(grid_shape)),
               'MU':     (conds_as_strings + ['component'], np.empty(grid_shape + (len(nonvacant_elements),))),
               'X':      (conds_as_strings + ['vertex', 'component'],
-                         np.empty(grid_shape + (len(nonvacant_elements)+1, len(nonvacant_elements),))),
+                         np.empty(grid_shape + (max_phases, len(nonvacant_elements),))),
               'Y':      (conds_as_strings + ['vertex', 'internal_dof'],
-                         np.empty(grid_shape + (len(nonvacant_elements)+1, maximum_internal_dof,))),
+                         np.empty(grid_shape + (max_phases, maximum_internal_dof,))),
               'Phase':  (conds_as_strings + ['vertex'],
-                         np.empty(grid_shape + (len(nonvacant_elements)+1,), dtype='U%s' % max_phase_name_len)),
+                         np.empty(grid_shape + (max_phases,), dtype='U%s' % max_phase_name_len)),
               'points': (conds_as_strings + ['vertex'],
-                         np.empty(grid_shape + (len(nonvacant_elements)+1,), dtype=np.int32))
+                         np.empty(grid_shape + (max_phases,), dtype=np.int32))
               }
-
-    specified_conds = set(conditions.keys())
-    unspecified_statevars = set(state_variables) - specified_conds
-    free_statevars = {str(x): [float(x.default_value)] for x in unspecified_statevars}
 
     for free_statevar, default_value in free_statevars.items():
         arrays.update({free_statevar: (conds_as_strings, np.full(grid_shape, default_value))})
